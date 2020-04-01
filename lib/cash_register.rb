b47cdac42a755c4b86ba5adb613dd31fde9e37ca
Mariam Haider <mfh.haider@gmail.com>
2020-04-01 02:12:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,11 @@ class CashRegister
   
   attr_accessor :total, :discount, :price, :item
   
-  def initialize(total, discount,)
+  def initialize(total, discount, price, item)
   @total = 0
   @discount = discount
+  @price = price
+  @item = []
   end 
   
   def total
