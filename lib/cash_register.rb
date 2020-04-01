bf630a9c93a1eefc9eed51e863c4450934c43a57
Mariam Haider <mfh.haider@gmail.com>
2020-04-01 02:07:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,1 @@
-class CashRegister
-  
-  attr_accessor :total, :discount
-  
-  def initialize(total, discount,)
-  @total = 0
-  @discount = discount
-  end 
-  
-  def total
-    @total
-  end 
-  
-  
-  
-end 
+
